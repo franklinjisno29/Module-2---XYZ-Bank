098617d7495f40bb2990240cf3ef438269380d4d
182278 <182278@ust.com>
2023-12-26 10:22:57 +0530

    26/12/23

--- XYZ Bank with BDD/Utilities/CoreCodes.cs
+++ XYZ Bank with BDD/Utilities/CoreCodes.cs
@@ -33,7 +33,7 @@ namespace XYZ_Bank_with_BDD.Utilities
             Log.Information(result);
             if (errorMessage == null)
             {
-                Log.Information(testName + "passed");
+                Log.Information(testName + "passed"); 
                 AllHooks.test.Pass(result);
             }
             else
@@ -41,7 +41,7 @@ namespace XYZ_Bank_with_BDD.Utilities
                 AllHooks.test.Fail(result);
                 Log.Error($"Test failed for {testName} \n Exception: \n{errorMessage}");
             }
-        } 
+        }  
         public static DefaultWait<IWebDriver> Waits(IWebDriver driver)
         {
             DefaultWait<IWebDriver> fluentWait = new DefaultWait<IWebDriver>(driver);
